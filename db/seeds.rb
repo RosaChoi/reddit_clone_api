175f2a8868da610e95cf3f3d30f803d9f22c048c
RosaChoi <rosa.uniq@gmail.com>
2015-02-22 12:53:09 -0800

    add serializer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create(
+  title: 'STUNNING Dog Tricks Performed by Ivy the American Eskimo',
+  username: 'momo',
+  link: 'http://youtu.be/-UIJvu5_ONk',
+  description: 'I love this video. American Eskimo rocks!'
+  )
+Post.create(
+  title: 'Watching These Pups Learn To Howl Makes Us Wish They’d Never Grow Up',
+  username: 'momo\'s mom',
+  link: 'http://youtu.be/eMF9HZU3K88',
+  description: 'ARRRRROOOOOOOOO!'
+  )
+Post.create(
+  title: '17 Corgis Who Are A Royal Pain In The Bum!',
+  username: 'monkey',
+  link: 'http://barkpost.com/uk-corgis-who-are-a-pain-in-the-bum/',
+  description: 'Go corgis!! I do the same to my human.'
+  )
